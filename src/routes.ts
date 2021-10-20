d21bb03c8def536f7fc4dbe931a0c80809a99799
Leo Miranda <contato@leomiranda.com>
2021-10-20 08:22:34 -0400

    feat: create message middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,14 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageControlle';
+import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 export const router = Router();
 
 router.post('/authenticate', new AuthenticateUserController().handle);
+
+router.post(
+	'messages',
+	ensureAuthenticated,
+	new CreateMessageController().handle
+);

317dfb49fccf1edeca1f85d25dbd7b9e865d4597
Leo Miranda <contato@leomiranda.com>
2021-10-19 22:22:10 -0400

    feat: return token user

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import prismaClient from '../prisma';
+import { sign } from 'jsonwebtoken';
 
 /**
  * Receive code (string)
@@ -37,7 +39,7 @@ export class AuthenticateUserService {
 				},
 			});
 
-		const { data } = await axios.get<IUserResponse>(
+		const response = await axios.get<IUserResponse>(
 			'https://api.github.com/user',
 			{
 				headers: {
@@ -46,6 +48,40 @@ export class AuthenticateUserService {
 			}
 		);
 
-		return data;
+		const { login, id, avatar_url, name } = response.data;
+
+		let user = await prismaClient.user.findFirst({
+			where: {
+				github_id: id,
+			},
+		});
+
+		if (!user) {
+			user = await prismaClient.user.create({
+				data: {
+					github_id: id,
+					login,
+					avatar_url,
+					name,
+				},
+			});
+		}
+
+		const token = sign(
+			{
+				user: {
+					name: user.name,
+					avatar_url: user.avatar_url,
+					id: user.id,
+				},
+			},
+			process.env.JWT_SECRET,
+			{
+				subject: user.id,
+				expiresIn: '1d',
+			}
+		);
+
+		return { token, user };
 	}
 }

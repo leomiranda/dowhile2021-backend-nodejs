207695e78740868550a40fa4c5f59e2ef8ea6547
Leo Miranda <contato@leomiranda.com>
2021-10-20 23:01:53 -0400

    feat: 3messages ProfileUser

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 export const router = Router();
@@ -12,3 +14,7 @@ router.post(
 	ensureAuthenticated,
 	new CreateMessageController().handle
 );
+
+router.get('/messages/last3', new GetLast3MessagesController().handle);
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
